151d7067430df898e2ed6c5ca0be4080b03d23b8
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2023-12-27 08:10:23 -0500

    Create UserService.java

---
+++ src/com/coderscampus/UserService.java
@@ -0,0 +1,9 @@
+package com.coderscampus;
+
+public class UserService {
+
+	//First, I want to read from the text file and import the users into an array to
+	//be used for validating our user input
+	
+	
+}
