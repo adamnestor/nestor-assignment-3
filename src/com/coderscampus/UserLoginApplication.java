278286d5422dcdbb46eee6388696712f3a639c27
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2023-12-28 16:06:57 -0500

    Update

--- src/com/coderscampus/UserLoginApplication.java
+++ src/com/coderscampus/UserLoginApplication.java
@@ -1,25 +1,59 @@
 package com.coderscampus;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class UserLoginApplication {
 
 	public static void main(String[] args) {
-		
-		UserService userService = new UserService();
-		User[] users;
-        try {
-            users = userService.getUsers("data.txt");
-        } catch (IOException e) {
-            System.out.println("Error reading file: " + e.getMessage());
-            return;
-        }
 
-        // Printing the returned users
-        System.out.println("List of Users:");
-        for (User user : users) {
-            System.out.println(user.getUsername() + " " + user.getPassword() + " " + user.getName());
-        }
+		// create an instance of UserService to allow use of its methods
+
+		UserService userService = new UserService();
+
+		// declares an array of users that belong to the User class
+		User[] users;
+
+		// this will read the file and assign created users to the users array, or it
+		// will tell us if an exception occurred
+		try {
+			users = userService.getUsers("data.txt");
+		} catch (IOException e) {
+			System.out.println("IOException: " + e.getMessage());
+			return;
+		}
+
+		// allow user to input a username and password
+		// validate the inputed username and password by checking if they are equal to
+		// existing user data
+
+		// set the number of attempts to 5
+		int maxAttempts = 5;
+
+		// create a scanner to allow for the input
+		Scanner keyboard = new Scanner(System.in);
+
+		// allow user to enter their data
+		int attempts = 0;
+		while (attempts < maxAttempts) {
+			System.out.println("Enter username: ");
+			String inputUsername = keyboard.nextLine();
+			System.out.println("Enter password: ");
+			String inputPassword = keyboard.nextLine();
+
+			// uses the method validateUser from userService class in order to compare the
+			// input and the stored data
+			User confirmUser = userService.validateUser(users, inputUsername, inputPassword);
+			if (confirmUser != null) {
+				System.out.println("Welcome " + confirmUser.getName());
+				return;
+			} else {
+				attempts++;
+				System.out.println("Invalid login, please try again.");
+			}
+
+		}
+		System.out.println("Too many failed login attempts, you are now locked out.");
 
 	}
 
--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -5,33 +5,44 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class UserService {
-	
+
+	// getUsers method reads the file and adds the lines into an array of users
 	public User[] getUsers(String fileName) throws IOException {
-		//reading the file
-		BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
-		
-		//counting the number of lines in file
+		// reading the file
+		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+		// counting the number of lines in file
 		String line;
 		int count = 0;
 		while (reader.readLine() != null) {
 			count++;
 		}
 		reader.close();
-		
-		//adding read lines to an array of users
+
+		// adding lines from file to an array of users
 		User[] users = new User[count];
 		reader = new BufferedReader(new FileReader(fileName));
-		
+
 		int index = 0;
-		
+
 		while ((line = reader.readLine()) != null) {
 			String[] data = line.split(",");
 			users[index++] = new User(data[0], data[1], data[2]);
 		}
 		reader.close();
-		
+
 		return users;
-		
+
 	}
-	
+
+	// validate username and password
+	public User validateUser(User[] users, String inputUsername, String inputPassword) {
+		for (User user : users) {
+			if (user.getUsername().equalsIgnoreCase(inputUsername) && user.getPassword().equals(inputPassword)) {
+				return user;
+			}
+		}
+		return null;
+	}
+
 }
